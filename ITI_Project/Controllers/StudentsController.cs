9720498c978b91e50d09b9672e1dec6aa6942b55
Rehab Hesham <rehabhesham98@yahoo.com>
2023-09-23 12:23:17 +0300

    Add project files.

---
+++ ITI_Project/Controllers/StudentsController.cs
@@ -0,0 +1,57 @@
+﻿using ITI_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI_Project.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public string GetName()
+        {
+            return "Ali";
+        }
+
+        public ContentResult GetName2()
+        {
+            ContentResult contentResult = new ContentResult();
+            contentResult.Content = "ali";
+            return contentResult;
+        }
+
+        public JsonResult GetJson()
+        {
+            JsonResult result = new JsonResult(new {id=10,name="ali"});
+            return result;
+        }
+
+        public ViewResult GetView()
+        {
+            ViewResult result = new ViewResult();
+            result.ViewName = "test"; 
+            return result;
+        }
+
+        public ActionResult GetData(int id) {
+            if ((id % 2) == 0)
+            {
+                return View("test");
+            }
+            else
+            {
+                return Content("not valid");
+            }
+        }
+
+        public ActionResult GetStudentData()
+        {
+            Student studentObj = new Student()
+            {
+                Id = 10,
+                Name = "ali",
+                Address = "Cairo",
+                Age = 17
+            };
+
+            return View("student",studentObj);
+        }
+    }
+}
---
+++ ITI_Project/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace ITI_Project.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Address: {Address}, Age: {Age}";
+        }
+    }
+}
